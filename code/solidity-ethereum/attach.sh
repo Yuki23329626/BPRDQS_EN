6ed3881c1ad95d38947fc86882dca6ad52715c33
Nong-Xiang Shen <michael7105@gmail.com>
2021-09-09 23:01:05 +0000

    debug

--- code/solidity-ethereum/attach.sh
+++ code/solidity-ethereum/attach.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
-container_id=`docker ps | grep ubuntu | cut -c1-12`
+container_id=`docker ps | grep geth-node | cut -c1-12`
 
 echo "entering bash of container: geth-node"
 echo "container_id = ${container_id}"
