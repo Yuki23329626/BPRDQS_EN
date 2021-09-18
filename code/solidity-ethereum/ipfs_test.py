99298444bfc99efd50cb4289636e7f97e1a279a5
Nong-Xiang Shen <michael7105@gmail.com>
2021-09-18 04:59:46 +0000

    infura

--- code/solidity-ethereum/ipfs_test.py
+++ code/solidity-ethereum/ipfs_test.py
@@ -1,2 +1,8 @@
 from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('https://rinkeby.infura.io/v3/3c8c82c3951c4cf3b61586b0202981c1'))
 
+print(w3.isConnected())
+
+w3.eth.get_block('latest')
+
+block = web3.eth.get_block('latest')
